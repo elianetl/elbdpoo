83a4d8336a1d51b02df5324180cbd28be4d5544c
Eliane Tâmara <eliane.tamara@academico.ifpb.edu.br>
2021-03-15 23:33:04 +0000

    Alteração para segurança do bd

--- src/conexao.js
+++ src/conexao.js
@@ -1,6 +1,6 @@
 let Sequelize = require("sequelize");
 
-let conexaodb = new Sequelize("postgres://vdxkttpacysbtu:b98bc6b458aac5590662854bd38acb10ac9b2cfacf3fc2cdb8db62916f72ef0f@ec2-54-167-168-52.compute-1.amazonaws.com:5432/d16c6kerm1l0gu", {
+let conexaodb = new Sequelize("", {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
